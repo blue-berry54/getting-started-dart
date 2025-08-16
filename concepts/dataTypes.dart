155d6d3125f3f1a10c6bc1f0b450cfba18d0adc9
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-16 23:33:55 +0530

    concepts updated

---
+++ concepts/dataTypes.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Integer
+  int integerValue1 = 50;
+  var integerValue2 = 100;
+
+  // Double
+  double doubleValue1 = 50.98;
+  var doubleValue2 = 68.89;
+
+  print('doubleValue1 = $doubleValue1 and doubleValue2 = $doubleValue2');
+  print('integerValue1 = $integerValue1 and integerValue2 = $integerValue2');
+}
