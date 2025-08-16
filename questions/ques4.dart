2017ed6beb8ad03dc8bfd97eb422662791db76a0
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-16 16:33:27 +0530

    modified structure

---
+++ questions/ques4.dart
@@ -0,0 +1,22 @@
+// Question 4
+//Use switch-case to print the day name based on a number (1-7)
+
+void main() {
+  int days = 2;
+  switch (days) {
+    case 1:
+      print("Sunday");
+    case 2:
+      print("Monday");
+    case 3:
+      print("Tuesday");
+    case 4:
+      print("Wednesday");
+    case 5:
+      print("Thusday");
+    case 6:
+      print("Friday");
+    case 7:
+      print("Saturday");
+  }
+}
